7c957e84f5a8c81073b45ee97929de6fce8f9b99
Ajelet Rivera <contact@quickcoding.io>
2016-12-03 21:11:47 -0800

    LICENSE

--- app/src/main/java/io/quickcoding/dropdownalert/MainActivity.java
+++ app/src/main/java/io/quickcoding/dropdownalert/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("QUICKCODING.IO","HELLOWORLD");
+        Log.e("QUICKCODING.IO","HELLO");
     }
 }
